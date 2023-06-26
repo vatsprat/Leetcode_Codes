d70a3695921bcb6c8750cffd711d4a978ce79ea3
Pratyush Vats <92757181+vatsprat@users.noreply.github.com>
2023-06-27 01:51:09 +0530

    Time: 0 ms (100%), Space: 6.2 MB (7.51%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    int bottomup(int n)
+    {
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        if(n==0)
+        {
+            return 0;
+        }
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
     int topdown(int n , vector<int> &dp)
     {
          if(n==1 || n==0)
@@ -18,8 +34,10 @@ public:
     }
     int fib(int n) 
     {
-       vector<int> dp(n+1,-1);
-       int ans = topdown(n,dp);
-       return ans;
+    //    vector<int> dp(n+1,-1);
+    //    int ans = topdown(n,dp);
+    //    return ans;
+
+        return bottomup(n);
     } 
 };

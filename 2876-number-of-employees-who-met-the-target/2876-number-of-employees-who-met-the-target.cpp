cfc6b79a2794f22ddf44e7b56cfb83ad89a6e21e
Pratyush Vats <92757181+vatsprat@users.noreply.github.com>
2023-08-09 01:03:35 +0530

    Time: 0 ms (100%), Space: 20.5 MB (37.48%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) 
+    {   
+        int count=0;
+        for(auto i:hours)
+        {
+            if(i>=target)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};

81f45648c452d2b9c5cb2ebff8a7908e9e303860
Pratyush Vats <92757181+vatsprat@users.noreply.github.com>
2023-08-17 00:04:07 +0530

    Time: 0 ms (100%), Space: 6.2 MB (5.69%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {
+        int result = 0 ;
+        for(int i=0;i<32;i++)
+        {
+            int lsb  = n & 1;
+            int reversed = lsb<<(31-i);
+            result = result | reversed;
+            n=n>>1;
+        }
+
+        return result;
+    }
+};
